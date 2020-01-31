c886fd059aa117a2d7f6bd5615776e3d157714eb
kritisingh1 <kritisingh1.ks@gmail.com>
2020-01-31 20:58:03 +0530

    display list of emails

--- app/src/main/java/com/example/mayday/ContactsAdapter.java
+++ app/src/main/java/com/example/mayday/ContactsAdapter.java
@@ -1,2 +1,50 @@
 package com.example.mayday;
 
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.MyViewHolder>{
+
+    private List<Contacts> contactsList;
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView name, email;
+
+        public MyViewHolder(View view) {
+            super(view);
+            name = (TextView) view.findViewById(R.id.name);
+            email = (TextView) view.findViewById(R.id.email);
+        }
+    }
+
+        public ContactsAdapter(List<Contacts> contactsList) {
+            this.contactsList = contactsList;
+        }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.contact_list_row, parent, false);
+
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        Contacts contact = contactsList.get(position);
+        holder.name.setText(contact.getName());
+        holder.email.setText(contact.getEmail());
+    }
+
+    @Override
+    public int getItemCount() {
+        return contactsList.size();
+    }
+
+}
--- app/src/main/java/com/example/mayday/DisplayEmail.java
+++ app/src/main/java/com/example/mayday/DisplayEmail.java
@@ -7,15 +7,52 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 
 import android.view.View;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DisplayEmail extends AppCompatActivity {
+
+    private List<Contacts> contactsList = new ArrayList<>();
+    private RecyclerView recyclerView;
+    private ContactsAdapter cAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_email);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+
+        cAdapter = new ContactsAdapter(contactsList);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(cAdapter);
+
+        prepareContactData();
+    }
+
+    private void prepareContactData() {
+        Contacts contact = new Contacts("abc", "abc@example.com");
+        contactsList.add(contact);
+
+        contact = new Contacts("def", "def@example.com");
+        contactsList.add(contact);
+
+        contact = new Contacts("ghi", "ghi@example.com");
+        contactsList.add(contact);
+
+        cAdapter.notifyDataSetChanged();
     }
 
 }
